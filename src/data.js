ccad36951d8ab2dab516dbbfaff8dc70cd53c8c6
siva29061997 <siva29061997@gmail.com>
2022-08-18 20:14:49 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "assets/2020-05-13-GettyImages-1214528250.jpg",
+    link: "https://playful-souffle-1a8f47.netlify.app",
+  },
+  {
+    id: 2,
+    img: "assets/360_F_142176822_NsvxLbEy3qndNFLTyI4x06u7ZpPAaeJo.jpg",
+    link: "https://delightful-marzipan-402913.netlify.app",
+  },
+  {
+    id: 3,
+    img: "assets/bVc4DkW.png",
+    link: "https://amazing-torrone-92fa9b.netlify.app",
+  },
+  {
+    id: 4,
+    img: "assets/images.png",
+    link: "https://glistening-sunflower-93f9fc.netlify.app",
+  },
+  {
+    id: 5,
+    img: "assets/Keval_Padia.webp",
+    link: "https://admirable-donut-687308.netlify.app",
+  },
+  {
+    id: 6,
+    img: "assets/istockphoto-1227056524-170667a.jpg",
+    link: "https://lively-pixie-6469d2.netlify.app",
+  },
+];
